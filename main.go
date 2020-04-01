9dee7fe9fd5d924d7aee9fe9af48f9fc53b61793
vashish1 <vashishtiv@gmail.com>
2020-04-01 23:44:54 +0530

    resolved cors

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func init(){
  cl1,cl2=db.Createdb()
 }
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/signup",signup).Methods("POST","GET")
